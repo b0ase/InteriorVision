51eada544975e20ff56e3ae2e0e4f2c564e59db6
b0ase <your-email@example.com>
2025-04-08 12:46:38 +0100

    Fix Tailwind darkMode configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: ["class"],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -93,6 +93,11 @@ const config: Config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
